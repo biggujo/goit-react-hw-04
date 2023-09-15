46933c5c9c10cdc6d038e714211ac1e093b073bd
Dmytro Ivashchenko <gen.style22@gmail.com>
2023-09-15 16:35:40 +0300

    Add API (Axios)

---
+++ src/services/api.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://pixabay.com/api';
+
+const API_KEY = '38308184-41247c978e0d2604524b8abfa';
+
+//https://pixabay.com/api/?q=cat&page=1&key=your_key&image_type=photo&orientation=horizontal&per_page=12
+
+export const fetchImages = async (query, page, perPage) => {
+  const urlParameters = new URLSearchParams({
+    key: API_KEY,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    q: query,
+    page,
+    per_page: perPage,
+  });
+
+  const response = await axios.get(`/?${urlParameters}`);
+
+  return response.data;
+};
